ea61428889db823133dff3c4901df6ae7768f8f0
hasintso2071 <74848587+hasintso2071@users.noreply.github.com>
2021-10-30 08:33:44 +0300

    Update accueil.php

--- view/accueil.php
+++ view/accueil.php
@@ -21,44 +21,54 @@
         <body>
             <div class="container-fluid">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="flex-grow: 2">
                  <a class="navbar-brand" href="#">
                      <img class="rounded float-left" alt="responsive image" src="../public/images/logo.png" right="60px" height="60px">
                  </a>
+                </div>
                      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                          <span class="navbar-toggler-icon"></span>
                     </button>
-                <div class="navbar-collapse collapse ml-5" id="navbarNavDropdown" style="">
-                  <ul class="navbar-nav ml-5">
-                     <li class="nav-item ml-5">
-                         <a class="nav-link" href="#back-end en attente">
-                             <i class="fas fa-arrow-circle-left" width="30px" height="30px" style="font-size: 34px;" aria-hidden="true"></i>
-                             <span class="sr-only"></span>
-                         </a>
-                     </li>
-                     <li class="nav-item">
-                         <a class="nav-link" href="#back-end en attente">
-                             <i class="fas fa-arrow-circle-right" width="30px" height="30px" style="font-size: 34px;" aria-hidden="true"></i>
-                         </a>
-                     </li>
-                     <li class="nav-item ml-5">
-                         <span class="navbar-text">
-                             <h1 class="text-success"> <strong>Mini Classroom</strong> </h1>
-                         </span>
-                     </li>
-                     <li class="nav-item dropdown">
-                         <a class="nav-link dropdown-toggle ml-5" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              <img class="rounded img-thumbnail" alt="responsive image" src="#back en attente">
-                         </a>
-                            <div class="dropdown-menu dropdown-content" aria-labelledby="navbarDropdownMenuLink">
-                                  <a class="dropdown-item" href="#">
-                                  <i class="fas fa-tools" aria-hidden="true">Paramètres</i>
-                                  </a>
-                                  <a class="dropdown-item" href="#">
-                                  <i class="fas fa-user" aria-hidden="true">Se déconnecter</i>
-                                  </a>                     
-                           </div>
-                     </li>
-                  </ul>
+                <div class="col-lg-9 col-8 flex-container id=" bs-example-navbar-collapse-1""="" style="font-size: 11px;letter-spacing: 0.325em;font-weight: 300;">
+                    <div class="navbar-collapse collapse" id="navbarNavDropdown" style="">
+                    <ul class="navbar-nav">
+                        <div class="flex-grow: 2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#back-end en attente">
+                                    <i class="fas fa-arrow-circle-left" width="30px" height="30px" style="font-size: 34px;" aria-hidden="true"></i>
+                                    <span class="sr-only"></span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#back-end en attente">
+                                    <i class="fas fa-arrow-circle-right" width="30px" height="30px" style="font-size: 34px;" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                        </div>
+                        <div class="flex-grow: 8">
+                            <li class="nav-item ml-5">
+                                <span class="navbar-text">
+                                    <h1 class="text-success"> <strong>Mini Classroom</strong> </h1>
+                                </span>
+                            </li>
+                        </div>
+                        <div class="flex-grow: 2">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle ml-5" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <img class="rounded img-thumbnail" alt="responsive image" src="#back en attente">
+                                </a>
+                                    <div class="dropdown-menu dropdown-content" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="#">
+                                        <i class="fas fa-tools" aria-hidden="true">Paramètres</i>
+                                        </a>
+                                        <a class="dropdown-item" href="#">
+                                        <i class="fas fa-user" aria-hidden="true">Se déconnecter</i>
+                                        </a>    
+                                    </div>                 
+                            </li>
+                        </div>
+                    </ul>
+                    </div>
                 </div>
             </nav>
                <div class="row" id="row_body">
@@ -87,7 +97,7 @@
                         </div> 
                    </div>
                </div>
-               <footer class="footer footer-expand-lg bg-dark footer-dark" height="200px">
+               <footer class="footer footer-expand-lg bg-dark footer-dark flex-container" height="200px">
                <div class="row text-center">
                    <div class="col-lg-4 col-sm-4 col-md-4 box">
                         <span class="copyright quick-links">
@@ -126,9 +136,11 @@
                 </footer> 
                        
             </div>
-    </body>
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+        
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+        
     </html>
